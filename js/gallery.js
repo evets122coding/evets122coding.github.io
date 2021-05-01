2c4c1b613f23e265272ece118b2b5f8f705c96ef
Steven Lufrano <evets122coding@gmail.com>
2021-05-01 16:25:21 -0400

    Preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,18 @@
 // Activates the image gallery.
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click.
-
-
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage  = document.querySelector("#gallery-photo > img");
+
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
 
@@ -23,7 +26,7 @@ function activateGallery() {
       let title       = galleryInfo.querySelector(".title");
       let description = galleryInfo.querySelector(".description");
 
-      title.innerHTML = thumbnail.dataset.title;
+      title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
   });
